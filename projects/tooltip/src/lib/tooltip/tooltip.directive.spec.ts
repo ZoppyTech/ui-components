8ff566a78b7f8f4fb8c518bb03a7c5aa7298533b
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2022-08-15 16:34:41 -0300

    Creating dropdown and tooltip components

--- projects/tooltip/src/lib/tooltip/tooltip.directive.spec.ts
+++ projects/tooltip/src/lib/tooltip/tooltip.directive.spec.ts
@@ -3,4 +3,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { TooltipDirective } from './tooltip.directive';
 
-describe('Directive: Tooltip', () => {});
+describe('Directive: Tooltip', () => {
+    it('tests placeholder', () => {
+        //TODO: should add tests to this directive
+        expect(true).toBe(true);
+    });
+});
